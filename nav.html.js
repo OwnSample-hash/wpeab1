165a2463480a68d1cfc7184c0ec9b8cd7d13ad7c
Ruskó Olivér <ownsample@tutanota.com>
2025-03-01 12:21:08 +0100

    Added rounded class

--- nav.html.js
+++ nav.html.js
@@ -4,4 +4,4 @@ pathId["/feladat-4/index.html"] = "chartjs";
 pathId["/feladat-5/ajax.html"] = "ajax";
 pathId["/feladat-6/index.html"] = "oojs";
 
-$(pathId[window.location.pathname]).classList.add("border-b-1");
+$(pathId[window.location.pathname]).classList.add("border-b-1", "rounded");
