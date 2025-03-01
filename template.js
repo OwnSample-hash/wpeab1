2e418bc34855a48f92acc0dd83618cdc5151f574
Ruskó Olivér <ownsample@tutanota.com>
2025-03-01 21:23:42 +0100

    Use 2 space indent

--- template.js
+++ template.js
@@ -1,34 +1,34 @@
 function $(id) {
-    return (id === "" ? null : document.getElementById(id));
+  return (id === "" ? null : document.getElementById(id));
 }
 
 async function template(id, file) {
-    await fetch(file)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("File not found");
-            }
-            return response.text();
-        })
-        .then(data => {
-            $(id).innerHTML = data;
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    await fetch(`${file}.js`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("File not found can be ignored");
-            }
-            return response.text();
-        })
-        .then(data => {
-            node = document.createElement("script");
-            node.innerHTML = data;
-            $(id).appendChild(node);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+  await fetch(file)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("File not found");
+      }
+      return response.text();
+    })
+    .then(data => {
+      $(id).innerHTML = data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  await fetch(`${file}.js`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("File not found can be ignored");
+      }
+      return response.text();
+    })
+    .then(data => {
+      node = document.createElement("script");
+      node.innerHTML = data;
+      $(id).appendChild(node);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
