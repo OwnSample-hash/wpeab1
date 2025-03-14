1c5749f953feede8af61d1cdd04e8eceefea9350
Ruskó Olivér <ownsample@tutanota.com>
2025-03-14 15:56:17 +0100

    Rename feladat-3 => feladat-2, format

--- nav.html.js
+++ nav.html.js
@@ -1,8 +1,9 @@
-pathId = []
+pathId = [];
 pathId["/"] = "home";
-pathId["/feladat-3/tabla.html"]="tabla";
+pathId["/feladat-2/tabla.html"] = "tabla";
 pathId["/feladat-4/index.html"] = "chartjs";
 pathId["/feladat-5/ajax.html"] = "ajax";
 pathId["/feladat-6/index.html"] = "oojs";
 
 $(pathId[window.location.pathname]).classList.add("border-b-1", "rounded");
+ 
