bf02c0aaa938b197543be3708ddceca4f606e420
Ruskó Olivér <ownsample@tutanota.com>
2025-04-12 11:56:54 +0200

    Reformat some stuff

--- feladat-4/util.js
+++ feladat-4/util.js
@@ -1,45 +1,49 @@
 function $(id) {
-  return (id === "" ? null : document.getElementById(id));
+  return id === "" ? null : document.getElementById(id);
 }
 
 nums = [];
 ch = null;
 
+const plugin = {
+  id: "customCanvasBackgroundColor",
+  beforeDraw: (chart, args, options) => {
+    const { ctx } = chart;
+    ctx.save();
+    ctx.globalCompositeOperation = "destination-over";
+    ctx.fillStyle = options.bg || "#99ffff";
+    ctx.fillRect(0, 0, chart.width, chart.height);
+    ctx.restore();
+  },
+};
+
 function onRowClick(row) {
   if (ch) {
     ch.destroy();
   }
-  const plugin = {
-    id: 'customCanvasBackgroundColor',
-    beforeDraw: (chart, args, options) => {
-      const { ctx } = chart;
-      ctx.save();
-      ctx.globalCompositeOperation = 'destination-over';
-      ctx.fillStyle = options.bg || '#99ffff';
-      ctx.fillRect(0, 0, chart.width, chart.height);
-      ctx.restore();
-    }
-  };
   ch = new Chart($("chart"), {
     type: "line",
     data: {
       labels: nums[row].map((_, i) => i + 1),
-      datasets: [{
-        label: "Row " + (row + 1),
-        data: nums[row],
-        borderColor: "#ccc",
-        fill: false,
-      }]
+      datasets: [
+        {
+          label: "Row " + (row + 1),
+          data: nums[row],
+          borderColor: "#ccc",
+          fill: false,
+        },
+      ],
     },
     options: {
       plugins: {
         customCanvasBackgroundColor: {
-          bg: "#1c1c1c"
-        }
-      }
+          bg: "#1c1c1c",
+        },
+      },
     },
-    plugins: [plugin]
+    plugins: [plugin],
   });
+  $("canvas").style.display = "";
 }
 
 function onload() {
@@ -53,9 +57,17 @@ function onload() {
       row = [];
     }
   }
-  $("tbl").innerHTML = nums.map(
-    (row, i) => `<tr class="tr" id="row-${i}">${row.map(
-      (num, j) => `<td id="cell-${i}-${j}" class="text" onclick="onRowClick(${i})">${num.toFixed(2)}</td>`)
-      .join("")}</tr>`)
+  $("tbl").innerHTML = nums
+    .map(
+      (row, i) =>
+        `<tr class="tr" id="row-${i}">${row
+          .map(
+            (num, j) =>
+              `<td id="cell-${i}-${j}" class="text" onclick="onRowClick(${i})">${num.toFixed(
+                2
+              )}</td>`
+          )
+          .join("")}</tr>`
+    )
     .join("");
 }
