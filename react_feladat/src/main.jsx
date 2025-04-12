b38f1dce4b111a11c5e12592b733ed1e62b5833d
Ruskó Olivér <ownsample@tutanota.com>
2025-04-12 13:46:30 +0200

    b2b

--- react_feladat/src/main.jsx
+++ react_feladat/src/main.jsx
@@ -4,14 +4,15 @@ import "./index.css";
 import Nav from "./nav";
 import Chat from "./chat/chat";
 import Calc from "./calc/calc";
+import B2B from "./b2b";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={window.location.replace("/")} />
       <Route path="/react/" element={<Nav />} />
       <Route path="/react/chat" element={<Chat />} />
       <Route path="/react/calc" element={<Calc />} />
+      <Route path="/" element={<B2B />} />
     </Routes>
   </BrowserRouter>
 );
