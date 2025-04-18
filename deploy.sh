0fb26f0b16a4bcd0745b94d57513e0cec026c4d9
Ruskó Olivér <ownsample@tutanota.com>
2025-04-18 13:20:59 +0200

    Deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $EUID -ne 33 ]]; then
+    echo "Please run this script as httpd user."
+    exit 1
+fi
+
+git reset --hard HEAD
+git pull
+rm -rf react
+mkdir react
+cd react_feladat
+npm i
+ln -s ../react dist
+npm run build
