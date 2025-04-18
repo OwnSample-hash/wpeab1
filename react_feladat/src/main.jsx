a71e3d0c2999765762f8b4a9e2560eb83b239a6b
Ruskó Olivér <ownsample@tutanota.com>
2025-04-18 14:58:43 +0200

    Landing page

--- react_feladat/src/main.jsx
+++ react_feladat/src/main.jsx
@@ -9,7 +9,15 @@ import B2B from "./b2b";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/react/" element={<Nav />} />
+      <Route
+        path="/react/"
+        element={
+          <div>
+            <Nav />
+            <h1>Web-programozás-1 Előadás Házi feladat</h1>
+          </div>
+        }
+      />
       <Route path="/react/chat" element={<Chat />} />
       <Route path="/react/calc" element={<Calc />} />
       <Route path="/" element={<B2B />} />
